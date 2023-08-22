6dcc41fd589f6090988a011559a7aec7520dd008
Aylin Deniz <aylinozunlu@gmail.com>
2023-08-22 09:27:21 +0300

    firstcommit

---
+++ employeemanager-backend/src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,35 @@
+package tech.getarrays.employeemanager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
---
+++ employeemanager-backend/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repo.EmployeeRepo;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Optional<Employee> findEmployeeById(Long id) {
+        return Optional.ofNullable(employeeRepo.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("Employee not found with id: " + id)));
+
+    }
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteById(id);
+    }
+
+
+}
